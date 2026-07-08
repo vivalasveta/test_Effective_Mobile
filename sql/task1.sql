1945be8bf74c8c6c2ba13ecc96832d150f99baf1
vivalasveta <vivalasveta@gmail.com>
2026-07-08 20:11:33 +0300

    Update task1.sql

--- sql/task1.sql
+++ sql/task1.sql
@@ -1,1 +1,9 @@
+SELECT 
+    id, 
+    scores,
+    RANK() OVER (ORDER BY scores DESC) AS position 
+FROM examination
+ORDER BY position, id
 
+#Использовала RANK(), так как при одинаковых баллах абитуриенты должны делить место (1, 1, 3, 4...).
+Если требуется нумерация без пропусков можно заменить на DENSE_RANK().
